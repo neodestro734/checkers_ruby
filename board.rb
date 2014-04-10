0d4053fb3275c533940652e8eb9fe54876793f24
Philip Ingram <neodestro734@yahoo.com>
2014-04-10 10:40:13 -0700

    add_piece, board_display (update), move_diffs, Piece#initialize (update)

--- board.rb
+++ board.rb
@@ -3,41 +3,61 @@ class Board
 	attr_accessor :rows
 
 	def initialize(filled = true)
-		@rows = Array.new(8) { Array.new(8, 'x') }
+		@rows = Array.new(8) { Array.new(8, nil) }
 	end
 
-	def [](x, y)
-		#these are swapped because rows (y) come before columns (x)
-		# x, y = pos
-		# @rows[7-y][x]
-		@rows[x][y]
+	def [](pos)
+		i, j = pos
+		@rows[i][j]
 	end
 
-	def []=(x, y, piece)
+	def []=(pos, piece)
 		# raise "invalid pos" unless valid_pos?(pos)
 
-		#these are swapped because rows (y) come before columns (x)
-    # x, y = pos
-    @rows[x][y] = piece
+		i, j = pos
+    @rows[i][j] = piece
+  end
+
+  def empty?(pos)
+  	return true if self[pos].nil?
+  	false
+  end
+
+  def on_board?(pos)
+  	pos.all? { |coord| (0..7).cover?(coord) }
   end
 
   def display
   	(0..7).each do |r|
   		(0..7).each do |c|
-  			# pos = [r, c]
-  			# unless self.rows[7-c][r].nil?
-  				print "#{self[r, c]} " #if self[r, c].nil?
-  			# end
+  			if self[[r, c]].nil?
+  				print ". "
+  			else
+  				print "#{self[[r, c]].disp_char} "
+  			end
   		end
   		print "\n"
   	end
   end
 
+  def add_piece(piece, pos)
+  	raise "position not empty" unless empty?(pos)
+
+  	self[pos] = piece
+  end
+
 end
 
-b = Board.new
-b.display
-puts "\n\n"
-b[0, 0] = 'a'
-b[5, 1] = 'f'
-b.display
+# b = Board.new
+# p b.on_board?([1,1])
+# p b.on_board?([7,7])
+# p b.on_board?([-1,1])
+# p b.on_board?([1,10])
+# p b.on_board?([10,1])
+# b.display
+# puts "\n\n"
+# b[[0, 0]] = 'a'
+# b[[5, 1]] = 'f'
+# b.display
+# p b.empty?([1,2])
+# p b.empty?([5,1])
--- piece.rb
+++ piece.rb
@@ -1,10 +1,68 @@
+require './board.rb'
+require 'debugger'
+
 class Piece
 
-	attr_accessor :color, :is_king
+	attr_accessor :color, :is_king, :pos, :board, :disp_char
 
-	def initialize(color)
+	def initialize(color, pos, board, is_king = false)
 		@color = color
-		@is_king = false
+		@pos = pos
+		@board = board
+		@is_king = is_king
+		@disp_char = 'P'
+
+		board.add_piece(self, pos)
+	end
+
+	def perform_jump(move_to)
+
+	end
+
+	def perform_slide(move_to)
+		# valid_moves = move_diffs.select { |diff| self.pos}
+	end
+
+	RED_MOVES = [[-1, -1], [-1, 1], [-2, -2], [-2, 2]]
+	BLACK_MOVES = [[1, -1], [1, 1], [2, -2], [2, 2]]
+	KING_MOVES = RED_MOVES + BLACK_MOVES
+
+	# private
+	def move_diffs
+		move_diffs = []
+
+		if self.is_king == false
+			if self.color == :red
+				move_diffs = RED_MOVES
+			elsif self.color == :black
+				move_diffs = BLACK_MOVES
+			end
+		else # self.is_king == true
+			move_diffs = KING_MOVES
+		end
+
+		valid_diffs = []
+
+		move_diffs.each do |diff|
+			new_pos = [@pos[0] + diff[0], @pos[1] + diff[1]]
+
+			if @board.on_board?(new_pos) && @board.empty?(new_pos)
+				valid_diffs << diff
+			end
+		end
+
+		valid_diffs
 	end
 
 end
+
+b = Board.new()
+piece = Piece.new(:black, [1, 1], b)
+p2 = Piece.new(:red, [2, 2], b)
+# p piece
+piece.move_diffs
+b.display
+# p piece.pos
+# p p2.pos
+# p piece
+# p p2

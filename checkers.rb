40685554ac008527968c38584d208de6c5e34909
Philip Ingram <neodestro734@yahoo.com>
2014-04-10 15:39:31 -0700

    basic piece movement from UI (no double-jumping)

--- checkers.rb
+++ checkers.rb
@@ -1,6 +1,7 @@
 require './board.rb'
 require './piece.rb'
 require 'debugger'
+require 'colorize'
 
 class Checkers
 
@@ -18,12 +19,16 @@ class Checkers
 			@board.display
 
 			piece_to_move = player.get_piece_to_move(@board)
+			if piece_to_move.color != player.color
+				puts "You must move your own piece!".colorize(:red)
+				next
+			end
 
 			# user_input = player.get_user_input
 			# parsed_input = parse_input(user_input)
-
-			# next unless piece_to_move.valid_move_seq?(parse_input)
-			# piece_to_move.perform_moves(move_sequence)
+			user_move_seq = [player.get_move_sequence]
+			next unless piece_to_move.valid_move_seq?(user_move_seq)
+			piece_to_move.perform_moves(user_move_seq)
 			# p piece_to_move.pos unless piece_to_move.nil?
 			# p player.color
 			player = (player.color == :red ? @player2 : @player1)
@@ -50,8 +55,8 @@ class HumanPlayer
 
 	def get_piece_to_move(board)
 		begin
-			puts 'Please enter a piece to move by its board coordinates (0,0)'
-			puts 'is in the upper left hand corner:'
+			puts 'Please enter a piece to move by its board coordinates.'
+			puts '(0,0) is in the upper left hand corner:'
 			piece_arr = gets.chomp.split(',')
 			piece_spot = piece_arr.map(&:to_i)
 			# debugger
@@ -65,14 +70,19 @@ class HumanPlayer
 		board[piece_spot]
 	end
 
-	def get_user_input
+	def get_move_sequence
+		move_seq = []
 		begin
-			puts 'Please enter a move sequence in this format: [[2,2],[4,4]]'
-			user_input = gets.chomp
-		rescue
+			puts 'Please enter a move sequence in this format: 2,2,4,4'
+			move_seq_arr = gets.chomp.split(',')
+			p move_seq_arr
+			move_seq = move_seq_arr.map(&:to_i)
+			p move_seq
+		rescue => e
+			puts e
 			retry
 		end
-		user_input
+		move_seq
 	end
 
 end
